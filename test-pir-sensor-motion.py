877456c5b7236c63d54f6b52c2b154a26528cfd4
Sathira Kittisukmongkol <nong.jamie.jamie@gmail.com>
2019-07-02 16:34:30 +0900

    [Updated]: Add some print

--- test-pir-sensor-motion.py
+++ test-pir-sensor-motion.py
@@ -2,6 +2,8 @@ from gpiozero import MotionSensor
 
 pir = MotionSensor(4)
 
+print('test-pir-sensor-motion.py has started ...')
+
 while True:
     pir.wait_for_motion()
     print('Motion detected!')
