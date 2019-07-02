6039f3901dcdc9a87e8e245d2fdc4ae163416d1f
varito <varit.as@ku.th>
2019-07-02 16:47:45 +0900

    fix store file

--- detect_intruder_record.py
+++ detect_intruder_record.py
@@ -9,8 +9,9 @@ while True:
     filename = "{0:%Y}-{0:%m}-{0:%d}".format(now)
     pir.wait_for_motion()
     print("Motion Detected!")
-    camera.capture('home/pi/video_'+ filename +'.png')
-
+    camera.start_recording(filename +'.h264')
+    pir.wait_for_no_motion
+    camera.stop_recording()
     question = input("Continue Monitoring? (Y)es or (N)o")
     if question == "N" or question == "n":
         print("Camera is closing...")
