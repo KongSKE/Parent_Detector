d8bfe42d6cf578825cd5ca8e3d565f5f608346aa
Sathira Kittisukmongkol <nong.jamie.jamie@gmail.com>
2019-07-10 15:46:49 +0900

    [Edited]: Rotate the camera

--- test-selfie-feature.py
+++ test-selfie-feature.py
@@ -10,6 +10,7 @@ camera = PiCamera()
 print('The test-selfie-feature starts.')
 print('Press the button to take a selfie')
 button.wait_for_press()
+camera.rotation = 180
 camera.capture('./testSelfieImage.jpg')
 print('Take a selfie!')
 camera.close()
