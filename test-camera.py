98c8d7f38ef9e4ebfa52e86290746b5cd27de52d
Sathira Kittisukmongkol <nong.jamie.jamie@gmail.com>
2019-07-10 15:39:08 +0900

    [Added]: Print among the process

--- test-camera.py
+++ test-camera.py
@@ -2,6 +2,14 @@ from picamera import PiCamera
 
 camera = PiCamera()
 
+print('The test-camera program starts.')
+
+print('Camera takes a photo name "selfie.png".')
+
 camera.capture('selfie.png')
 
+print('The camera closes.')
+
 camera.close()
+
+print('The program ends.')
