db751ad2303ba33516cb6713b1859ae8f866cd0b
Sathira Kittisukmongkol <nong.jamie.jamie@gmail.com>
2019-07-10 15:59:33 +0900

    [Added]: Test stop motion feature

---
+++ test-stop-motion.py
@@ -0,0 +1,23 @@
+from picamera import PiCamera
+from gpiozero import Button
+
+# Initialized
+camera = PiCamera()
+button = Button(17)
+
+# Stop motion
+print('The test-stop-motion starts.')
+print('The program will automatically exit when counter = 5.')
+counter = 1
+max = 5
+while counter <= max:
+    try:
+        print('Counter = ' + str(counter))
+        button.wait_for_press()
+        camera.capture('/home/pi/Desktop/Parent_Detector/animation/frame%03d.jpg' % counter)
+        print('Take the picture, ' + str(counter) + ' round.')
+        counter += 1
+    except:
+        break
+print('The test-stop-motion finishes.')
+print('The program ends.')
