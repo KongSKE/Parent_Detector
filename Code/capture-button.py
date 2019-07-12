9df1176bf08e4eece288e622160cab210fe21651
Sathira Kittisukmongkol <nong.jamie.jamie@gmail.com>
2019-07-12 15:30:37 +0900

    [Edited]: Comment the camera preview and change the path of destination

--- Code/capture-button.py
+++ Code/capture-button.py
@@ -8,12 +8,12 @@ camera = PiCamera()
 camera.rotation = 180
 
 # Take the picture by clicking the button.
-camera.start_preview()
+# camera.start_preview()
 while True:
     try:
         button.wait_for_press()
         pic_name = str(datetime.datetime.now())[:19:].replace(':', '-').replace(' ','_')
-        camera.capture('/home/pi/Parent_Detector/button-pic/' + pic_name + '.jpg')
+        camera.capture('../button-pic/' + pic_name + '.jpg')
     except KeyboardInterrupt:
         camera.stop_preview()
         break
